b979230e1d7c2ec6867894165bfe2e6adeaeb582
Bruno Leão <leaodebrito@icloud.com>
2023-05-01 19:57:04 -0300

    modelo teste e modelo treino

--- Projeto 1 - Housing/caracterizacao_dados.py
+++ Projeto 1 - Housing/caracterizacao_dados.py
@@ -1,3 +1,7 @@
+# Autor: Bruno Leão de Brito
+# Data: 2023-05-01
+
+# Importando bibliotecas
 import pandas as pd
 import numpy
 import seaborn as sns
@@ -7,18 +11,20 @@ import matplotlib.pyplot as plt
 file_path = 'Projeto 1 - Housing/datasets/housing.csv'
 housing = pd.read_csv(file_path, sep=',')
 
-'''
+
 #analise de caracteristicas do dataset
 housing.info()
-'''
+
 
 housing_valueCounts = housing['ocean_proximity'].value_counts()
+print(housing_valueCounts)
 
 describe_housing = housing.describe()
+print(describe_housing)
 
 #plotagem de gráficos histograma
-sns.histplot(housing['median_income'])
+#sns.histplot(housing['median_income'])
 
 #plotagem de gráficos de dispersão
-sns.scatterplot(data = housing, x = 'longitude', y = 'latitude', hue = 'median_income', palette = 'viridis', alpha = 0.2)
+#sns.scatterplot(data = housing, x = 'longitude', y = 'latitude', hue = 'median_income', palette = 'viridis', alpha = 0.2)
 #plt.show()
--- Projeto 1 - Housing/organizacao_dataset.py
+++ Projeto 1 - Housing/organizacao_dataset.py
@@ -1,10 +1,22 @@
-from caracterizacao_dados import housing
+# Autor: Bruno Leão de Brito
+# Data: 2023-05-01
+
+# Importando bibliotecas
 import pandas as pd
 import numpy as np
 import sklearn  as sk
 
-print(housing.head())
+#Importando arquivos locais
+file_path = 'Projeto 1 - Housing/datasets/housing.csv'
+housing = pd.read_csv(file_path, sep=',')
 
 #Separando o dataset em treino e teste
 from sklearn.model_selection import train_test_split
 train_set, test_set = train_test_split(housing, test_size = 0.2, random_state=42)
+
+print(train_set.head())
+print(test_set.head())
+
+#Salvando datasets de treino e teste em arquivos csv
+train_set.to_csv('Projeto 1 - Housing/datasets/train_set_housing.csv', index=False)
+test_set.to_csv('Projeto 1 - Housing/datasets/test_set_housing.csv', index=False)
