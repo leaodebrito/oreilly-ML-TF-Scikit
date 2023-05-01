d7821e45598efad2dfbd4bd3e4c149c3ce8487a5
Bruno Leão <leaodebrito@icloud.com>
2023-05-01 19:07:30 -0300

    atualização de readme

--- importar_dados.py
+++ importar_dados.py
@@ -3,7 +3,12 @@ import numpy as np
 import urllib
 from urllib import request
 
+#URL do arquivo
 file_url = 'https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.csv'
+
+#Local onde o arquivo será salvo
 file = 'datasets/housing.csv'
 
+#Método para baixar o arquivo
 request.urlretrieve(file_url, file)
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Aprendizado de máquina com Scikit Learn e Tensor Flow
+
